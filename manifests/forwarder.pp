004b985e26ab0824f1f0e329cfd3036719652267
Mike Farrow <contact@mikefarrow.co.uk>
2015-03-25 20:55:26 +0000

    adding anchors

--- manifests/forwarder.pp
+++ manifests/forwarder.pp
@@ -1,15 +1,19 @@
 class log::forwarder {
 
-	class { 'beaver': }
+	anchor { 'log::forwarder::begin': } ->
+
+	class { 'beaver': } ->
 
 	file_fragment { 'logstash_version_fragment' :
 		tag     => "beaver_config_${::fqdn}",
 		content => "logstash_version:1\n",
 		order   => 11,
-	}  
+	} ->
 
 	beaver::output::redis { 'redis_out':
 		host => hiera('log::broker', 'localhost'),
-	}
+	} ->
+
+	anchor { 'log::forwarder::end': }
 
 }
