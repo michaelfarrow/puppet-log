b0bbf9c30f5d32ddc3bd6305ef4d9267a4a1c3aa
Mike Farrow <contact@mikefarrow.co.uk>
2015-03-25 21:25:52 +0000

    adding hostname

--- manifests/forwarder.pp
+++ manifests/forwarder.pp
@@ -2,7 +2,17 @@ class log::forwarder {
 
 	anchor { 'log::forwarder::begin': } ->
 
-	class { 'beaver': } ->
+	if $cond_agentfqdn != '' {
+		$host = $cond_agentfqdn
+	} elsif $cond_masterfqdn != '' {
+		$host = $cond_masterfqdn
+	} else {
+		$host = $::fqdn
+	}
+
+	class { 'beaver':
+		hostname => $host,
+	} ->
 
 	file_fragment { 'logstash_version_fragment' :
 		tag     => "beaver_config_${::fqdn}",
